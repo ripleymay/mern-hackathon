0fd01e125d8a816689233fac5f328ff30dcd824d
Jim Clark <jim.clark@generalassemb.ly>
2021-11-29 10:28:41 -0800

    add useEffect

---
+++ models/item.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+// Ensure that the Category model is loaded by Mongoose 
+// so that it can be used to populate the items
+require('./category');
+const itemSchema = require('./itemSchema');
+
+module.exports = mongoose.model('Item', itemSchema);
--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,5 +1,23 @@
+import { useState, useEffect } from 'react';
+
 export default function NewOrderPage() {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(function() {
+    console.log('NewOrderPage rendered');
+  });
+  
+  useEffect(function() {
+    console.log('NewOrderPage rendered for the first time');
+  }, []);
+  // the empty dependency array above will result in 
+  // the function running after the FIRST render
+  // only
+
   return (
-    <h1>NewOrderPage</h1>
+    <>
+      <h1>NewOrderPage</h1>
+      <button onClick={() => setMenuItems(menuItems + 1)}>Trigger re-render</button>
+    </>
   );
 }
