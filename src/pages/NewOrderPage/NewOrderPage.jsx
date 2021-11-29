233e87efd03bcf4a744c8678ab04318c839d14da
Jim Clark <jim.clark@generalassemb.ly>
2021-11-29 11:39:46 -0800

    Finish useEffect & useRef lesson

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,8 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
+import './NewOrderPage.css';
+import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
-export default function NewOrderPage() {
+export default function NewOrderPage({ user, setUser }) {
   const [menuItems, setMenuItems] = useState([]);
+  const [activeCat, setActiveCat] = useState('');
+  // Obtain a ref object
+  const categoriesRef = useRef([]);
 
   // useEffect(function() {
   //   console.log('NewOrderPage rendered');
@@ -11,6 +21,11 @@ export default function NewOrderPage() {
   useEffect(function() {
     async function getItems() {
       const items = await itemsAPI.getAll();
+      categoriesRef.current = items.reduce((cats, item) => {
+        const cat = item.category.name;
+        return cats.includes(cat) ? cats : [...cats, cat]
+      }, []);
+      setActiveCat(categoriesRef.current[1]);
       setMenuItems(items);
     }
     getItems();
@@ -20,9 +35,21 @@ export default function NewOrderPage() {
   // only
 
   return (
-    <>
-      <h1>NewOrderPage</h1>
-      <button onClick={() => setMenuItems(menuItems + 1)}>Trigger re-render</button>
-    </>
+    <main className="NewOrderPage">
+      <aside>
+        <Logo />
+        <CategoryList
+          categories={categoriesRef.current}
+          activeCat={activeCat}
+          setActiveCat={setActiveCat}
+        />
+        <Link to="/orders" className="button btn-sm">PREVIOUS ORDERS</Link>
+        <UserLogOut user={user} setUser={setUser} />
+      </aside>
+      <MenuList
+        menuItems={menuItems.filter(item => item.category.name === activeCat)}
+      />
+      <OrderDetail />
+    </main>
   );
 }
