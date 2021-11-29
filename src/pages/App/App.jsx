e471e740dd38f65ab87b1466008d778a71eef91f
Jim Clark <jim.clark@generalassemb.ly>
2021-11-29 15:20:38 -0800

    starter-code for mern-shopping-cart-part-1

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
@@ -15,6 +15,7 @@ export default function App() {
           {/* client-side route that renders the component instance if the path matches the url in the address bar */}
           <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
           <Route path="/orders" element={<OrderHistoryPage />} />
+          <Route path="/" element={<Navigate to="/orders/new" />} />
         </Routes>
         :
         <AuthPage setUser={setUser} />
