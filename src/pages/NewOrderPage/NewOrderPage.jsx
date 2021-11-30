0f9e2beaf357949c8f8348e881476ab1576fd796
Jim Clark <jim.clark@generalassemb.ly>
2021-11-30 11:07:11 -0800

    update

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -43,6 +43,15 @@ export default function NewOrderPage({ user, setUser }) {
   // the function running after the FIRST render
   // only
 
+  /*--- Event Handlers ---*/
+
+  async function handleAddToOrder(itemId) {
+    // 1. Call the addItemToCart function in ordersAPI, passing to it the itemId, and assign the resolved promise to a variable named cart.
+    const updatedCart = await ordersAPI.addItemToCart(itemId);
+    // 2. Update the cart state with the updated cart received from the server
+    setCart(updatedCart);
+  }
+
   return (
     <main className="NewOrderPage">
       <aside>
@@ -57,6 +66,7 @@ export default function NewOrderPage({ user, setUser }) {
       </aside>
       <MenuList
         menuItems={menuItems.filter(item => item.category.name === activeCat)}
+        handleAddToOrder={handleAddToOrder}
       />
       <OrderDetail order={cart} />
     </main>
